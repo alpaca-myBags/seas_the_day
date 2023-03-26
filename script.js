96b0ac28f455ac2a5c6fb0d09356827a88515a8c
ivoherre <i.herrera98@hotmail.com>
2023-03-25 20:34:15 -0400

    checking for excursion

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ const fetchData = async (url) => {
     console.log(error);
   }
 };
+
+document.addEventListener("DOMContentLoaded", async () => {
+  console.log("updated content");
+  const excursionAPIUrl = " http://api.opentripmap.com/0.1/ru/places/bbox?lon_min=38.364285&lat_min=59.855685&lon_max=38.372809&lat_max=59.859052&kinds=churches&format=geojson&apikey=xxxxx"
+
+  const data = await fetchData(excursionAPIUrl);
+    console.log(data);
+  });
+
+  const fetchData1 = async (url) => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
